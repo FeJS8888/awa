d2cb89ddf36d206193a663664ecf697a9867d1ea
FeJS8888 <fmxfmx2007@163.com>
2022-12-29 09:29:41 +0800

    Test

--- test/scripts/MainLib.js
+++ test/scripts/MainLib.js
@@ -84,5 +84,5 @@ world.events.beforeChat.subscribe((chat) => {
     }
 })
 log("KAKA")
-File.readFrom("replay/place/FeJS8888.rep",true)
+// File.readFrom("replay/place/FeJS8888.rep",true)
 if (!world.scoreboard.getObjective("BedWars")) world.scoreboard.addObjective("BedWars", "BedWars")
